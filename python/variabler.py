c29d7a6e1626778caa52bd4cf997575a7421174c
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2020-09-02 15:48:10 +0200

    printing. variabler. input/output.

---
+++ python/variabler.py
@@ -0,0 +1,11 @@
+name = "Lucian"   #Har skapat variablen name och tilldelat det värdet Lucian
+age = 17  #skapat variablen age och tilldelat det värdet 29
+print(f"Hello {name} you are {age} year old")
+
+side = float(input("Ange kvadratens sida"))
+
+area = side**2
+
+omkrets = 4*side
+
+print(f"kvadratens area är {area} a.e. och kvadraten omkrets är {omkrets} l.e.")
