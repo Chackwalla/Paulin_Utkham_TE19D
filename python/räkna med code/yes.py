904d504f86fbdbb67294303fcfacaee261e4dc33
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2020-09-07 10:51:15 +0200

    First assignment

---
+++ python/räkna med code/yes.py
@@ -0,0 +1,30 @@
+print("Nivå 1")
+
+print("upg 1 a: ", 12-7)
+
+print("upg 1 b: ", 24/3+9 )
+
+print("upg 1 c: ", 3*43-2)
+
+print("upg 2: Hej! hur mår du?")
+
+läsk = 15
+
+burgare = 29
+
+fries = 12
+
+morötter = 12
+
+print(f"upg 3 a: {2*läsk}")
+
+print(f"upg 3 b: {2*läsk+burgare}")
+
+print(f"upg 3 c: {3*läsk+3*burgare+3*fries}")
+
+print(f"upg 4: ", 219*45+8)
+
+print("upg 5: ", )
+print("Om du kör med hastigheten 70km/h i 1,5 timmar, så hinner du")
+print(70*1.5)
+print("km.")
