11925c80c1916508fb0d557bb2f6ea4082a6e12f
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2020-11-19 10:56:30 +0100

    moment04 a klar

--- TiaTiloos/moment04.py
+++ TiaTiloos/moment04.py
@@ -1,2 +1,24 @@
 # Inlämmningsuppgift moment 04
 
+
+
+
+
+    # Mata in rektangelns two sidor
+
+    kort = 5 # int(input("Korta sidan"))
+
+    long = 5 # int(input("långa sidan"))
+
+
+    # beräkna och skriv ut rektangelns area
+
+    print(kort*long)
+
+
+    # om båda sidorna är lika långa
+    if kort == long:
+        print("Detta är en kvadrat")
+
+    else:
+        print("Detta är en rektangel")
