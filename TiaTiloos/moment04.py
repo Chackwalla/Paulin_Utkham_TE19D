4c678db14fea79e52f092214c922ef386ebe1481
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2020-11-24 11:09:59 +0100

    försöker göra b

--- TiaTiloos/moment04.py
+++ TiaTiloos/moment04.py
@@ -1,14 +1,16 @@
 # Inlämmningsuppgift moment 04
 
+import random as rnd
 
+stop = 7 # int(input("How many loops do you wanna do?"))
 
-
+for i in range(1,stop):
 
     # Mata in rektangelns two sidor
 
     kort = 5 # int(input("Korta sidan"))
 
-    long = 5 # int(input("långa sidan"))
+    long = 4 # int(input("långa sidan"))
 
 
     # beräkna och skriv ut rektangelns area
@@ -22,3 +24,5 @@
 
     else:
         print("Detta är en rektangel")
+
+print (i)
