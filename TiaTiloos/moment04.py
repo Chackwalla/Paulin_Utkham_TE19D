d879b783c222e709b560536a831c32f1b27cb79e
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2020-11-26 09:17:37 +0100

    I'm trying my best on the last assignment

--- TiaTiloos/moment04.py
+++ TiaTiloos/moment04.py
@@ -2,27 +2,35 @@
 
 import random as rnd
 
-stop = 7 # int(input("How many loops do you wanna do?"))
+
+stop = 3 # int(input("How many loops do you wanna do?"))
 
 for i in range(1,stop):
 
     # Mata in rektangelns two sidor
 
-    kort = 5 # int(input("Korta sidan"))
+    kort = 5 #int(input("Korta sidan"))
 
-    long = 4 # int(input("långa sidan"))
+    long = 4 #int(input("långa sidan"))
 
 
     # beräkna och skriv ut rektangelns area
+    area=kort*long
 
-    print(kort*long)
+    print(area,end=" ")
 
 
-    # om båda sidorna är lika långa
+    # om båda sidorna är lika långa ...
     if kort == long:
-        print("Detta är en kvadrat")
+        print("Detta är en kvadrat")   # ... tala om att det är en kvadrat
 
-    else:
-        print("Detta är en rektangel")
+    # Loopa höjden från 1 till 10
+    for j in range(1,10):
+        volume = j*area  # Beräkna volymen och skriv ut den
+        print(f"Volymen {j} av rektangeln är {volume}")
+    #else:
+     #   print("Detta är en rektangel")
+
+    
 
 print (i)
